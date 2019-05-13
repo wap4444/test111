9469e7018a0e55608cc8259f40eb74f0b0ba122f
wap4444 <wap4444@mail.ru>
2019-05-14 03:02:24 +0600

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+		
+  
+        function didReceiveRemoteNotificationCallBack(jsonData) {
+}
+        function didOpenRemoteNotificationCallBack(jsonData) {
+}
+        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
+        var iosSettings = {};
+        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
+        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
+        //ПОДКЛЮЧЕНИЕ ПУШЕЙ
+           window.plugins.OneSignal
+          .startInit("299d98c7-7b0e-4fb3-81cc-c0fb349cb21a")
+          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
+          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
+              .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
+          .iOSSettings(iosSettings)
+          .endInit();
+        window.plugins.OneSignal.getIds(function(ids) {
+            ipush = ids.userId;
+			console.log('11121 '+ipush)
+            localStorage.ipush=ipush;
+			
+            });
+
+		
+		
+    }
+};
